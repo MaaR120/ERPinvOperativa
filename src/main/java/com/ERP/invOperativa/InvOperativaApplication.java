9c26601de3cdee3622e022c21f5494f00000f9a2
MaaR120 <martineguren120@gmail.com>
2024-05-27 17:25:03 -0300

    PrimerCommit

---
+++ src/main/java/com/ERP/invOperativa/InvOperativaApplication.java
@@ -0,0 +1,13 @@
+package com.ERP.invOperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvOperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvOperativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ERP/invOperativa/InvOperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ERP.invOperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvOperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
