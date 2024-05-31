2200e2e78a9863f0bbe615c4b64c2675f926e423
PauRodriguezz <paurodriguez028@gmail.com>
2024-05-31 13:20:02 -0300

    dependencia thymeleaf, atributos en articulos

--- src/main/java/com/ERP/invOperativa/Entities/Articulo.java
+++ src/main/java/com/ERP/invOperativa/Entities/Articulo.java
@@ -1,9 +1,59 @@
 package com.ERP.invOperativa.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
 
 @Entity
+@Table(name= "articulo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Articulo extends Base{
 
-    //PRUEBA DE PUSH
+    @NotNull
+    private String NombreArticulo;
+
+    private Double CostoAlmacenamiento;
+
+    private int Stock;
+
+    private int StockSeguridad;
+
+    private int PuntoPedido;
+
+    private int LoteOptimo;
+
+    @NotNull
+    private double precio;
+
+    private Date fechaBaja;
+
+    @ManyToOne
+    @JoinColumn(name="FamiliaArticulo")
+    private FamiliaArticulo familiaArticulo;
+
+
+    // Métodos para calcular stockSeguridad, puntoPedido, loteOptimo
+    public int calcularStockSeguridad() {
+        // retornar el calculo
+        return 0;
+    }
+
+    public int calcularPuntoPedido() {
+        // retornar el calculo
+        return 0;
+    }
+
+    public int calcularLoteOptimo() {
+        // retornar el calculo
+        return 0;
+    }
 }
