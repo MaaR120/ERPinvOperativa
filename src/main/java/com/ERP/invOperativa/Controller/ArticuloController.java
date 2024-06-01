1429ea453d76b2f4d95e24a67fab5c5685fcef70
PauRodriguezz <paurodriguez028@gmail.com>
2024-06-01 12:21:24 -0300

    CrudMaestroArticulo

--- src/main/java/com/ERP/invOperativa/Controller/ArticuloController.java
+++ src/main/java/com/ERP/invOperativa/Controller/ArticuloController.java
@@ -1,4 +1,40 @@
 package com.ERP.invOperativa.Controller;
 
+import com.ERP.invOperativa.Entities.Articulo;
+import com.ERP.invOperativa.Services.ArticuloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class ArticuloController {
+    @Autowired
+    private ArticuloService service;
+
+    @GetMapping("/maestroarticulo")
+    public String listarArticulos(Model modelo) {
+        modelo.addAttribute("articulos", service.ListarArticulos());
+        return "MaestroArticulo"; // nos retorna al archivo html MaestroArticulo
+    }
+
+    @GetMapping("/maestroarticulo/nuevo")
+    public String formularioCreararticulo(Model modelo){
+        Articulo articulo = new Articulo();
+        modelo.addAttribute("articulo", articulo);
+        return "crear_articulo";
+    }
+
+    @PostMapping("/articulos")
+    public String saveArticulo(@ModelAttribute("articulo") Articulo articulo){
+        service.saveArticulo(articulo);
+        return "redirect:/maestroarticulo";
+    }
+
+    @GetMapping("/maestroarticulo/{id}")
+    public String eliminarArticulo(@PathVariable Long id){
+        service.deleteArticulo(id);
+        return "redirect:/maestroarticulo";
+    }
+
 }
--- src/main/java/com/ERP/invOperativa/Services/ArticuloService.java
+++ src/main/java/com/ERP/invOperativa/Services/ArticuloService.java
@@ -2,5 +2,12 @@ package com.ERP.invOperativa.Services;
 
 import com.ERP.invOperativa.Entities.Articulo;
 
+import java.util.List;
+
 public interface ArticuloService extends BaseService<Articulo,Long> {
+    public List<Articulo> ListarArticulos();
+
+    public Articulo saveArticulo(Articulo articulo);
+
+    public Articulo deleteArticulo(Long id);
 }
