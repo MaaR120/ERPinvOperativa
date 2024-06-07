f6837b04713bc9cebfccbe383c4759c251b1ca92
GiulianaOzan <giuliana.ozan@gmail.com>
2024-06-07 19:43:30 -0300

    Persistencia BD

--- src/main/java/com/ERP/invOperativa/InvOperativaApplication.java
+++ src/main/java/com/ERP/invOperativa/InvOperativaApplication.java
@@ -36,33 +36,33 @@ public class InvOperativaApplication {
 		return args -> {
 			System.out.println("------------------------Estoy Funcionando------------------------");
 
-			// Crear artículos
-			Articulo articulo1 = new Articulo();
-			articulo1.setNombreArticulo("Tornillo");
-			articulo1.setPrecio(2500.0);
-			articuloRepository.save(articulo1);
-
-			Articulo articulo2 = new Articulo();
-			articulo2.setNombreArticulo("Tuerca");
-			articulo2.setPrecio(1000);
-			articuloRepository.save(articulo2);
-
-			// Crear detalles de venta
-			DetalleVenta detalleVenta1 = new DetalleVenta();
-			detalleVenta1.setArticulo(articulo1);
-			detalleVenta1.setCantidad(5);
-			detalleVenta1.setSubtotal(detalleVenta1.getCantidad() * detalleVenta1.getArticulo().getPrecio());
-
-			List<DetalleVenta> detallesVenta = new ArrayList<>();
-			detallesVenta.add(detalleVenta1);
-
-			// Crear venta
-			Venta venta = new Venta();
-			venta.setDetalleVentas(detallesVenta);
-			venta.setTotalVenta(detallesVenta.getLast().getSubtotal());
-			venta.setFechaFacturacion(new Date(System.currentTimeMillis()));
-
-			ventaRepository.save(venta);
+//			// Crear artículos
+//			Articulo articulo1 = new Articulo();
+//			articulo1.setNombreArticulo("Tornillo");
+//			articulo1.setPrecio(2500.0);
+//			articuloRepository.save(articulo1);
+//
+//			Articulo articulo2 = new Articulo();
+//			articulo2.setNombreArticulo("Tuerca");
+//			articulo2.setPrecio(1000);
+//			articuloRepository.save(articulo2);
+//
+//			// Crear detalles de venta
+//			DetalleVenta detalleVenta1 = new DetalleVenta();
+//			detalleVenta1.setArticulo(articulo1);
+//			detalleVenta1.setCantidad(5);
+//			detalleVenta1.setSubtotal(detalleVenta1.getCantidad() * detalleVenta1.getArticulo().getPrecio());
+//
+//			List<DetalleVenta> detallesVenta = new ArrayList<>();
+//			detallesVenta.add(detalleVenta1);
+//
+//			// Crear venta
+//			Venta venta = new Venta();
+//			venta.setDetalleVentas(detallesVenta);
+//			venta.setTotalVenta(detallesVenta.getLast().getSubtotal());
+//			venta.setFechaFacturacion(new Date(System.currentTimeMillis()));
+//
+//			ventaRepository.save(venta);
 
 
 		};
