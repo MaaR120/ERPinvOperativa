14ba428a3abf235e92f5e72157b601dfaf0ee5a7
MaaR120 <martineguren120@gmail.com>
2024-06-10 01:35:22 -0300

    PM 1era version

--- src/main/java/com/ERP/invOperativa/Services/PrediccionDemandaImpl.java
+++ src/main/java/com/ERP/invOperativa/Services/PrediccionDemandaImpl.java
@@ -4,11 +4,14 @@ import com.ERP.invOperativa.DTO.DTOPrediccion;
 import com.ERP.invOperativa.DTO.RequestPrediccionDemanda;
 import com.ERP.invOperativa.DTO.VentasPorMesDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
+@Service
 public class PrediccionDemandaImpl implements PrediccionDemanda{
     @Autowired
     protected VentaServiceImpl ventaService;
@@ -21,10 +24,33 @@ public class PrediccionDemandaImpl implements PrediccionDemanda{
         calendar.setTime(fechaInicio);
         calendar.add(Calendar.MONTH, -3);
         Date fechaIni=calendar.getTime();
+        try {
+            List<VentasPorMesDTO> ventasPorMes=ventaService.obtenerVentasPorMes(fechaIni,fechaInicio,articuloId);
+            List<DTOPrediccion> prediccionList=new ArrayList<>();
+            for (VentasPorMesDTO ventasPorMesDTO:ventasPorMes){
+                DTOPrediccion prediccion=new DTOPrediccion();
+                prediccion.setMes(ventasPorMesDTO.getMes());
+                prediccion.setCantidadReal(ventasPorMesDTO.getCantidad());
+                prediccionList.add(prediccion);
+            }
+            DTOPrediccion prediccion=new DTOPrediccion();
+            // Crear un SimpleDateFormat con el patrón 'yyyy-MM'
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
+            calendar.setTime(fechaInicio);
+            calendar.add(Calendar.MONTH, +1);
+            Date fechaPrediccion=calendar.getTime();
+            prediccion.setMes(dateFormat.format(fechaPrediccion));
+            int cantidadPrediccion=0;
+            for (DTOPrediccion prediccion1:prediccionList){
+                cantidadPrediccion+=prediccion1.getCantidadReal();
+            }
+            cantidadPrediccion=cantidadPrediccion/3;
+            prediccion.setCantidadPrediccion(cantidadPrediccion);
+            prediccionList.add(prediccion);
+            return prediccionList;
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
 
-        List<VentasPorMesDTO> ventasPorMesDTOS=ventaService.obtenerVentasPorMes(fechaIni,fechaInicio,articuloId);
-
-
-        return null;
     }
 }
